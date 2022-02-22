3c751dd45d71b17a6920a9c70415a6555230e938
Micha Alt <mail@lean-stack.de>
2022-02-22 08:45:35 +0100

    Add first two samples. Starter code only.

---
+++ samples/slots/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Slots mit v-slot</h1>
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+header {
+  padding: 1rem 2rem;
+  border-bottom: 1px solid var(--vt-c-divider-light-1);
+}
+
+main {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+</style>
---
+++ samples/two-way-binding/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <h1>Two-Way Binding mit v-model</h1>
+  </header>
+  <main>
+    <article>
+      <h2>Einfaches Two-Way Binding</h2>
+    </article>
+    <article>
+      <h2>Multiple Two-Way Binding</h2>
+    </article>
+  </main>
+</template>
+
+<style scoped>
+
+header {
+  padding: 1rem 2rem;
+  border-bottom: 1px solid var(--vt-c-divider-light-1);
+}
+
+main {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+</style>
